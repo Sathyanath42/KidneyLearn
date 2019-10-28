d747f6f7e0cf758c903c92fd48a031be2a710a1e
Sathyanath42 <43042869+Sathyanath42@users.noreply.github.com>
2019-10-28 13:57:17 -0400

    Decision tree first edition
    Extraction of data and execution using decision tree

---
+++ DecisiontreeFinal.py
@@ -0,0 +1,43 @@
+import sklearn
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import shuffle
+from sklearn import metrics
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("Cleankidney.txt")
+"""print(data.head())"""
+
+le = preprocessing.LabelEncoder()
+age = le.fit_transform(list(data["age"]))
+al = le.fit_transform(list(data["al"]))
+bu = le.fit_transform(list(data["bu"]))
+sc = le.fit_transform(list(data["sc"]))
+htn = le.fit_transform(list(data["htn"]))
+dm = le.fit_transform(list(data["dm"]))
+cls = le.fit_transform(list(data["class"]))
+predict = "class"
+
+x = list(zip(age, al, bu, sc, htn, dm))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
+
+clf = DecisionTreeClassifier()
+
+# Train Decision Tree Classifer
+clf = clf.fit(x_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(x_test)
+
+names = ["ckd", "notckd"]
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
+
+for x in range(len(y_pred)):
+    print("predicted:", names[y_pred[x]], "Data:", x_test[x], "actual:", names[y_test[x]])
