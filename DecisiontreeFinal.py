0c525386ce5d6d5f7e6aff39a4377f3c9a76d2f6
Sathyanath Nandakumar <43042869+Sathyanath42@users.noreply.github.com>
2019-11-01 00:03:19 -0400

    Update DecisiontreeFinal.py

--- DecisiontreeFinal.py
+++ DecisiontreeFinal.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model, preprocessing
 
-def process_split(dt, ts):
+def process_split(dt, ts):  #function for fitting the values and then splitting the values into a training and test set
     dtc = list(dt.columns.values)
     label = preprocessing.LabelEncoder()
     for i in dtc:
@@ -17,17 +17,17 @@ def process_split(dt, ts):
     xt, xts, yt, yts = sklearn.model_selection.train_test_split(xc, yc, test_size=ts)
     return xt, xts, yt, yts
 
-def DT_gini(xg, yg):
+def DT_gini(xg, yg): #gini classifier function
     clf_g = DecisionTreeClassifier(criterion="gini",random_state=100, max_depth=3, min_samples_leaf=5)
     clf_g.fit(xg, yg)
     return clf_g
 
-def DT_entr(xe, ye):
+def DT_entr(xe, ye): #entropy classifier function
     clf_e = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=3, min_samples_leaf=5)
     clf_e.fit(xe, ye)
     return clf_e
 
-kdata = pd.read_csv("Cleankidney.txt")
+kdata = pd.read_csv("Cleankidney.txt")  
 datahead = list(kdata.columns.values)
 
 x_train, x_test, y_train, y_test = process_split(kdata, 0.2)
@@ -47,9 +47,9 @@ y_pred = clf_er.predict(x_test)
 
 names = ["ckd", "notckd"]
 
-acc = metrics.accuracy_score(y_test, y_pred)
+acc = metrics.accuracy_score(y_test, y_pred) #printing the accuracy
 print(acc)
 
-for x in range(len(y_pred)):
+for x in range(len(y_pred)):  #printing out the expected and predicted values
     print("predicted:", names[y_pred[x]], "Data:", x_test[x], "actual:", names[y_test[x]])
 
